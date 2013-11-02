c3774c36944a424d22f8515be424e3e8b4d1b10c
Eric Haase <erichaase@gmail.com>
2013-11-02 03:29:20 -0500

    Remove non-printable characters from JSON string

--- box_score.rb
+++ box_score.rb
@@ -8,7 +8,7 @@ class BoxScore
   BS_URI = "http://scores.espn.go.com/nba/gamecast12/master?xhr=1&gameId=%s&lang=en&init=true&setType=true&confId=null"
 
   def initialize (gid)
-    json = JSON.parse(open(BS_URI % gid.to_s).read)
+    json = JSON.parse(open(BS_URI % gid.to_s).read.scan(/[[:print:]]/).join)
 
     bses = json['gamecast']['stats']['player']['home'][0...-1] + json['gamecast']['stats']['player']['away'][0...-1]
     @bses = []
